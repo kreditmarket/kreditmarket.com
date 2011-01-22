221b45032dcf825186e21ed252274c49f28b99a9
Adam Miribyan <kreditmarket.com@gmail.com>
2011-01-22 13:00:22 +0300

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+KreditmarketCom::Application.config.secret_token = '0994c28d5fcd1e03ab4e0582310fab2250b8d8b1017c079aa93ca9bebebe391d5439962faac24570217bc6b4ba8770c2637411e169ee20aa37416487bc9adce6'
