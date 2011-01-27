c95c7921c06eab30d525e74506a47ca68799bef0
Adam Miribyan <kreditmarket.com@gmail.com>
2011-01-27 12:03:15 +0300

    request form validation

--- app/models/request.rb
+++ app/models/request.rb
@@ -1,2 +1,6 @@
 class Request < ActiveRecord::Base
+  validates :personal_last_name, :presence => true
+  validates :personal_first_name, :presence => true
+  validates :personal_surname, :presence => true
+  
 end
