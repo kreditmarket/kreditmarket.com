001e35801c9b40fccac1107360424895c6672c92
Adam Miribyan <kreditmarket.com@gmail.com>
2011-02-09 16:47:16 +0300

    switched emails

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -5,13 +5,13 @@ class UserMailer < ActionMailer::Base
   
   def loan_request_to_office(request)
     @request = request
-    # mail(:to => "magazinkreditov@mail.ru", :subject => "Новая онлайн заявка")
-    mail(:to => "adam.miribyan@gmail.com", :subject => "Новая онлайн заявка manager")
+    mail(:to => "magazinkreditov@mail.ru", :subject => "Новая онлайн заявка")
+    # mail(:to => "adam.miribyan@gmail.com", :subject => "Новая онлайн заявка manager")
   end
   
   def loan_request_to_director(request)
     @request = request
-    # mail(:to => "kreditmarket.com@yandex.ru", :subject => "Новая онлайн заявка")
-    mail(:to => "adam.miribyan@gmail.com", :subject => "Новая онлайн заявка admin")
+    mail(:to => "kreditmarket.com@yandex.ru", :subject => "Новая онлайн заявка")
+    # mail(:to => "adam.miribyan@gmail.com", :subject => "Новая онлайн заявка admin")
   end
 end
