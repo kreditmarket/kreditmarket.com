3b3cbebf7acd14c2d1f7c2c0bb684e7b1111931f
Adam <adam.miribyan@gmail.com>
2011-02-25 16:48:28 +0300

    agent settings page now works fine

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,10 +1,19 @@
+#coding: utf-8
+
 class UsersController < Clearance::UsersController
 	before_filter :authenticate, :only => [:show, :edit]	
-	before_filter :define_the_user
-
+	before_filter :define_the_user, :only => [:show, :edit, :update]
 	
 	def edit
 		
+	end
+	
+	def update
+		if @user.update_attributes(params[:user])
+      redirect_to settings_path, :notice => "Настройки успешно сохранены."
+    else
+      render :template => 'users/edit'
+    end
 	end
 	
 	def show
