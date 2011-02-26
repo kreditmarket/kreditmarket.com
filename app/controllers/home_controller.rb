211e9277b6539d6dc9fba1d2f66a87f8a1c66066
Adam <adam.miribyan@gmail.com>
2011-02-27 02:33:16 +0300

    improved an izhd count field default value

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -2,6 +2,7 @@ class HomeController < ApplicationController
   def index
     @request = Request.new
     @request.agent_code = params[:c].to_i if params[:c]
+    @request.personal_izhd_count = 0
   end
 
 end
