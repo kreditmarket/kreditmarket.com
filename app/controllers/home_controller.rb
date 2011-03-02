f5d3d369ce121265a1cf169af3f79cc315b86081
Adam <adam.miribyan@gmail.com>
2011-03-02 04:43:44 +0300

    added a plans and pricing table to the main page under the planstable var

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -3,6 +3,12 @@ class HomeController < ApplicationController
     @request = Request.new
     @request.agent_code = params[:c].to_i if params[:c]
     @request.personal_izhd_count = 0
+    
+    if params[:planstable]
+    	@show_plans_table = true
+  	else
+  		@show_plans_table = false
+		end
   end
 
 end
