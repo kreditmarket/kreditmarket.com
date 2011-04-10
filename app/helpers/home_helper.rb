96519a8d2a103e54e38bc03710ab20e927b73af8
Adam Miribyan <kreditmarket.com@gmail.com>
2011-04-10 17:19:29 +0400

    other branches link helper

--- app/helpers/home_helper.rb
+++ app/helpers/home_helper.rb
@@ -1,3 +1,5 @@
+# encoding: utf-8
+
 module HomeHelper
 	def url_for_affiliate
 		if signed_in?
@@ -10,5 +12,17 @@ module HomeHelper
 			page_path("affiliate")
 		end
 	end
+  
+  def other_branches_link
+    if params[:controller] == "home"
+      "<div class=\"other-branches\">
+        ↓ <a href=\"#branches\" class=\"pseudo\">другие филиалы</a>
+       </div>".html_safe
+    else
+      "<div class=\"other-branches\">
+        <a href=\"/#branches\">другие филиалы</a>
+       </div>".html_safe
+    end
+  end
 	
 end
