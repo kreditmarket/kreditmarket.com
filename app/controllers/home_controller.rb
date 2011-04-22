a9b448db9dbfd30daffefb417b6c5d3271885be7
Adam Miribyan <kreditmarket.com@gmail.com>
2011-04-23 01:23:33 +0400

    branches list on the main page

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -3,6 +3,7 @@ class HomeController < ApplicationController
     @request = Request.new
     @request.agent_code = params[:c].to_i if params[:c]
     @request.personal_izhd_count = 0
+    @offices = Office.all
   end
 
 end
